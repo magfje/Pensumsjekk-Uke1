c4475f4a81cbce4448e77827f2df40e4144c91d8
Magnus Fjelldal <122229520+magfje@users.noreply.github.com>
2023-05-02 14:25:06 +0200

    Add project files.

---
+++ PensumsjekkUke1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace PensumsjekkUke1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.OutputEncoding = Encoding.UTF8;
+            var go = true;
+            while (go) {
+                Console.WriteLine("Write a name");
+                var n1 = Console.ReadLine();
+                if (n1 == null) {
+                    return;
+                } else {
+                    Console.WriteLine(n1 + " is a " + RandomHobby() + "\r\n");
+                }
+            }
+        }
+
+        static string RandomHobby() {
+            string[] hobbys = { "ninja🥷", "politi👮", "brannmann🧑‍🚒", "trollmann🧙‍♂️", "lege👨‍⚕️", "astronaut👨‍🚀" };
+
+            Random rnd = new();
+            int randHobby = rnd.Next(minValue: 0, hobbys.Length - 1);
+            return hobbys[randHobby];
+        }
+    }
+}
